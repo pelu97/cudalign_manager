d0b62349af984f8e8fcea8c4e5d515973722b55e
Pedro Lucas <pedrolpandrade@gmail.com>
2022-07-20 13:11:40 -0300

    Base file with definitions to be used by all other files

---
+++ src/base.h
@@ -0,0 +1,12 @@
+// General definitions to be used by various/all files
+
+// Work directory
+#ifndef WORK_DIR
+#define WORK_DIR "/work.tmp"
+#endif
+
+
+// Debug flag - enables debug prints inside modules
+// #ifndef DEBUG
+// #define DEBUG
+// #endif
