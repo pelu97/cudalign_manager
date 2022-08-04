a561352cf7ceb52323fbadafa0210fc15743b480
pelu97 <pedrolpandrade@gmail.com>
2022-08-04 11:57:49 -0300

    Added directories for the Laico lab machine; -The directories will be changed int he future. THe cudalign executables and the sequences will be moved inside the project folder and will be bundled with the project.

--- src/base.h
+++ src/base.h
@@ -5,8 +5,36 @@
 #define WORK_DIR "/work.tmp"
 #endif
 
+// CUDAlign directories
+#ifndef CUDA64_DIR
+#define CUDA64_DIR "/home/laicoadm/Documentos/Pedro/MASA_CUDAlign_64_threads"
+#endif
+
+#ifndef CUDA128_DIR
+#define CUDA128_DIR "/home/laicoadm/Documentos/Pedro/MASA_CUDAlign_128_threads"
+#endif
+
+#ifndef CUDA256_DIR
+#define CUDA256_DIR "/home/laicoadm/Documentos/Pedro/MASA_CUDAlign_256_threads"
+#endif
+
+
+// Sequence directories
+#ifndef SEQ_DIR
+#define SEQ_DIR "/home/laicoadm/Documentos/Pedro/Sequences"
+#endif
+
+
+//Sequence File Names
+#ifndef SEQ_1_A
+#define SEQ_1_A "AE002160.2.fasta"
+#endif
+#ifndef SEQ_1_B
+#define SEQ_1_B "CP000051.1.fasta"
+#endif
+
 
 // Debug flag - enables debug prints inside modules
-// #ifndef DEBUG
-// #define DEBUG
-// #endif
+#ifndef DEBUG
+#define DEBUG
+#endif
